f51b2ed9b4ced6318f69e2f166c705edbffc3299
ShadowLp174 <samuel.moravec@yahoo.com>
2026-04-01 20:12:43 +0000

    moved search.js to mjs file

---
+++ commands/search.mjs
@@ -0,0 +1,47 @@
+import { CommandBuilder } from "../src/CommandHandler.mjs";
+import { MessageHandler } from "../src/MessageHandler.mjs";
+
+function awaitMessage(msg, count, player) {
+  /** @type {MessageHandler} */
+  const messages = this.messages;
+  const channel = messages.getChannel(msg.channel.id);
+  const unobserve = channel.onMessageUser((m) => {
+    if (m.content.trim().toLowerCase() == "x") {
+      unobserve();
+      return m.replyEmbed("Cancelled!");
+    }
+    let c = parseInt(m.content.trim().replace(/\./g, ""));
+    if (isNaN(c)) return m.replyEmbed("Invalid number! (Send 'x' to cancel)");
+    if (c < 0 || c > count) return m.replyEmbed("Index out of range! (`1 - " + count + "`)");
+    let v = player.playResult(msg.authorId, c - 1);
+    m.replyEmbed((typeof v == "string") ? v : `Added [${v.title}](${v.url}) to the queue!`);
+    unobserve();
+  }, msg.author);
+}
+
+export const command = new CommandBuilder()
+  .setName("search")
+  .setDescription("Display the search results for a given query", "commands.search")
+  .addExamples("$prefixsearch never gonna give you up", "$prefixsearch -provider yt 'never gonna give you up'")
+  .addChoiceOption(o =>
+    o.setName("provider")
+      .setDescription("The search result provider (YouTube, YouTube Music or SoundCloud). Default: SoundCloud", "options.search.provider")
+      .addChoices("yt", "ytm", "scld")
+      .setDefault("ytm")
+      .addFlagAliases("p", "u", "use"), true)
+  .addTextOption(o =>
+    o.setName("query")
+      .setDescription("The query to search for.", "options.search.query")
+      .setRequired(true)
+  );
+export const run = async function (msg, data) {
+  const p = await this.getPlayer(msg);
+  if (!p) return;
+  let query = data.get("query").value;
+  let provider = data.get("provider")?.value;
+  msg.replyEmbed("Loading results...").then(async m => {
+    let res = await p.fetchResults(query, msg.authorId, provider);
+    m.editEmbed(res.m);
+    awaitMessage.call(this, msg, res.count, p);
+  });
+};
